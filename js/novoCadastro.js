276082e27dacff11df95d636a7cd0fbec4d4d9f1
MatheusSanches02 <rm87426@fiap.com.br>
2021-05-28 20:10:53 -0300

    Melhoria d ecodigo paginas html e adiçao de arquivo JS

--- js/novoCadastro.js
+++ js/novoCadastro.js
@@ -1,2 +1,31 @@
 // Pagina JavaScript referente ao cadastro de novo usuario
+var user;
+var password;
 
+/*
+
+
+function validar()
+{
+    inputUser = document.getElementById('');
+    inputPassword = document.getElementById('');
+    if(inputUser = user && inputPassword == password)
+    {
+        window.location = "faleConosco.html";
+    }
+    else
+    {
+        alert("Usuario não cadastrado!");
+    }
+}
+*/
+
+function creatUser()
+{
+    inputUser = document.getElementById('idTxtNome');
+    inputPassword = document.getElementById('idTxtSenha');
+
+    this.user = inputUser;
+    this.password = inputPassword;
+
+}
