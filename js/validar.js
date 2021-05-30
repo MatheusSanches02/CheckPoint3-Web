2165bc0b09d17bb7f49356be1788239377a3fae6
MatheusSanches02 <rm87426@fiap.com.br>
2021-05-30 14:48:22 -0300

    Validação em JS finalizada!

--- js/validar.js
+++ js/validar.js
@@ -5,8 +5,8 @@ Turma TDSG
 Toda logica foi feita em JS tirando validação de preenchimento de campos, feito em HTML
 Antes das linhas de codigo existem comantarios documentando sobre o que foi feito no projeto
 */
-
-const listUser = [
+const listUser = 
+[
     {
         user: "Giovanna",
         password: 87019
@@ -30,12 +30,20 @@ const listUser = [
 ];
 
 // validação de login com integrantes do grupo ja adicionados ao cadastro
-function validar()
+function sign()
 {
-    const inputUsuario = document.getElementById("idTxtUsuarioLogin").value;
-    const inputSenha = document.getElementById("idTxtSenhaLogin").value;
-    const exist = listUser.find(x => x.user == inputUsuario && x.password == inputSenha)
+    const inputUsuario = document.getElementById("idTxtUsuarioLogin");
+    const inputSenha = document.getElementById("idTxtSenhaLogin");
+    
+    // Validando os campos obrigaórios
+    const listElements = 
+    [
+        { element: inputUsuario, errorMessageId: 'message-error-user' },
+        { element: inputSenha, errorMessageId: 'message-error-password' },
+    ];
+    if (this.verifyErrorsRequired(listElements)) return;
 
+    const exist = listUser.find(x => x.user == inputUsuario.value && x.password == inputSenha.value);
     if(exist)
     {
         alert("Login!!")
@@ -50,23 +58,28 @@ function validar()
 // realiza o cadastro de usuario 
 function createUser()
 {
-    inputUser = document.getElementById('idTxtNomeCadastro').value;
-    inputPassword = document.getElementById('idTxtSenhaCadastro').value;
-    if(inputUser != "" && inputPassword != "")
+    inputUser = document.getElementById('idTxtNomeCadastro');
+    inputPassword = document.getElementById('idTxtSenhaCadastro');
+    
+    const listElements = 
+    [
+        { element: inputUser, errorMessageId: 'message-error-user' },
+        { element: inputPassword, errorMessageId: 'message-error-password' },
+    ];
+    if (this.verifyErrorsRequired(listElements)) return;
+    
+    const exist = listUser.find(x => x.user == inputUser.value);
+    if(exist)
     {
-        const exist = listUser.find(x => x.user == inputUser);
-        if(exist)
-        {
-            alert('Já existe um usuario com esse nome.');
-        }
-        else
-        {
-            const user = { user: inputUser, password: inputPassword };
-            listUser.push(user);
-            
-            alert(`Usuário ${user.user} foi cadastrado com sucesso.`);
-            this.showListUsers();
-        }
+        alert('Já existe um usuario com esse nome.');
+    }
+    else
+    {
+        const user = { user: inputUser.value, password: inputPassword.value };
+        listUser.push(user);
+        
+        alert(`Usuário ${user.user} foi cadastrado com sucesso.`);
+        this.showListUsers();
     }
 }
 
@@ -85,10 +98,18 @@ function showListUsers()
 
 function talkUs()
 {
-    const subject = document.getElementById('idTxtAssuntoFalar').value;
-    const firstName = document.getElementById('idTxtNomeFalar').value;
-    const email = document.getElementById('idTxtEmail').value;
-    const comments = document.getElementById('idTxtComentario').value;
+    const subject = document.getElementById('idTxtAssuntoFalar');
+    const firstName = document.getElementById('idTxtNomeFalar');
+    const comments = document.getElementById('idTxtComentario');
+
+    // Validando os campos obrigaórios
+    const listElements = 
+    [
+        { element: subject, errorMessageId: 'message-error-subject' },
+        { element: firstName, errorMessageId: 'message-error-name' },
+        { element: comments, errorMessageId: 'message-error-password' },
+    ];
+    if (this.verifyErrorsRequired(listElements)) return;
 
     if(subject == "" || firstName == "" || comments == "")
     {
@@ -99,4 +120,45 @@ function talkUs()
         alert('Todos os campos obrigaórios foram preenchidos!')
         window.location = "../html/portal.html"
     }
+}
+
+function verifyErrorsRequired(_listElements)
+{
+    let countError = 0;
+    for (const item of _listElements)
+    {
+        if (this.isEmpty(item.element.value))
+        {
+            this.setErrorToElement(item.element, item.errorMessageId);
+            countError = countError + 1;
+        }
+        else
+        {
+            this.removeErrorToElement(item.element, item.errorMessageId);
+        }
+    }
+
+    return countError > 0;
+}
+
+function isEmpty(_value)
+{
+    if (!_value)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+function setErrorToElement(_element, _messageErrorId)
+{
+    _element.classList.add('input-error');
+    document.getElementById(_messageErrorId).classList.remove('hide');
+}
+
+function removeErrorToElement(_element, _messageErrorId)
+{
+    _element.classList.remove('input-error');
+    document.getElementById(_messageErrorId).classList.add('hide');
 }
